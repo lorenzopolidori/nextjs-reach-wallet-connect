38592c3e600a1178911d48a3ea4dcd3bc9ad8b8c
Lorenzo Polidori <lorenzopolidori@gmail.com>
2022-04-01 12:10:25 +0100

    Upgrade to reach 0.1.9-rc.6

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,19 @@
 import Head from 'next/head'
-import { useRef, useState, useEffect } from 'react'
+import { loadStdlib } from '@reach-sh/stdlib';
+import MyAlgoConnect from '@reach-sh/stdlib/ALGO_MyAlgoConnect';
+const stdlib = loadStdlib({ ...process.env, 'REACH_CONNECTOR_MODE': 'ALGO' });
 
 export default function Home() {
-  const reach = useRef()
-  const myAlgoConnect = useRef()
-  const walletConnect = useRef()
-  const [loading, setLoading] = useState(true)
-
   async function connectMyAlgoWallet() {
-    setWallet({ MyAlgoConnect: myAlgoConnect.current })
+    setWallet({ MyAlgoConnect: MyAlgoConnect })
     await connectWallet()
   }
 
   async function connectPeraWallet() {
-    setWallet({ WalletConnect: walletConnect.current })
-    await connectWallet()
   }
 
   function setWallet(wallet) {
-    reach.current.setWalletFallback(reach.current.walletFallback({
+    stdlib.setWalletFallback(stdlib.walletFallback({
       providerEnv: 'TestNet', ...wallet
     }))
   }
@@ -26,35 +21,14 @@ export default function Home() {
   async function connectWallet() {
     let account;
     try {
-      account = await reach.current.getDefaultAccount()
+      account = await stdlib.getDefaultAccount()
     } catch (e) {
       console.error('Error when connecting to wallet', e)
       return;
     }
-    const balance = await reach.current.balanceOf(account)
-    reach.current.formatCurrency(balance, 4)
+    const balance = await stdlib.balanceOf(account)
+    stdlib.formatCurrency(balance, 4)
   }
-
-  useEffect(() => {
-    async function loadLibs() {
-      let [reachStdlib, myAlgoConnectLib] = await Promise.all([
-        import('@reach-sh/stdlib'),
-        import('@reach-sh/stdlib/ALGO_MyAlgoConnect')
-      ])
-      reach.current = reachStdlib.loadStdlib({ ...process.env, 'REACH_CONNECTOR_MODE': 'ALGO' })
-      myAlgoConnect.current = myAlgoConnectLib.default
-
-      try {
-        let walletConnectLib = await import('@reach-sh/stdlib/ALGO_WalletConnect')
-        walletConnect.current = walletConnectLib.default
-      } catch (e) {
-        console.error('Error when loading ALGO_WalletConnect', e)
-      }
-
-      setLoading(false)
-    }
-    loadLibs()
-  }, [])
 
   return (
     <div className="container">
@@ -68,13 +42,8 @@ export default function Home() {
         <header>NextJS React web application to allow users to connect to their Algorand wallets using the reach client library.</header>
 
         <nav>
-          {loading && <div>Loading...</div>}
-          {!loading &&
-            <>
-              <button onClick={connectMyAlgoWallet}>Connect to MyAlgo Wallet</button>
-              <button onClick={connectPeraWallet}>Connect to Pera Wallet</button>
-            </>
-          }
+          <button onClick={connectMyAlgoWallet}>Connect to MyAlgo Wallet</button>
+          <button onClick={connectPeraWallet}>Connect to Pera Wallet</button>
         </nav>
       </main>
     </div>
